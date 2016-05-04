d76fa28f93141c32d4abb6396eb607de69b9d266
Joao Antonio Silva <joao@gmail.com>
2016-05-04 11:46:16 -0300

    Script load data and plot charts

---
+++ script.R
@@ -0,0 +1,73 @@
+getMac <- function(dir){
+    fileMac <- gsub(" ", "", paste(dir, "/tables_MAC"))
+    mac <- read.csv(fileMac, header = FALSE)
+}
+
+getMic <- function(dir){
+    fileMic <- gsub(" ", "", paste(dir, "/tables_MIC"))
+    mic <- read.csv(fileMic, header = FALSE)
+}
+
+getTime <- function(dir){
+    fileTime <- gsub(" ", "", paste(dir, "/tables_Time"))
+    time <- read.csv(fileTime,  sep=";", header = FALSE)
+}
+
+getNC <- function(dir){
+    fileNC <- gsub(" ", "", paste(dir, "/tables_NewClass"))
+    nc <- read.csv(fileNC, header = FALSE)
+}
+
+getDatFrame <- function(dir){
+    
+    dataFrame <- data.frame(x1 = character(), x2 = numeric(), x3 = numeric(), x4 =  numeric(),x5 = numeric(), x6 = numeric(), x7 = numeric())
+    
+    listDir <- list.dirs(dir)
+    algs <- read.csv("algorithms", header = FALSE )
+    j <- 1
+    for (dir1 in listDir[2:4]) {
+        
+        # print(dir1)
+        
+        mac <- getMac(dir1)
+        mic <- getMic(dir1)
+        time <- getTime(dir1)
+        nc <- getNC(dir1)
+        
+        i = 1
+        
+        while(i < length(mac$V1)+1){
+            dataFrame <- rbind(dataFrame, data.frame(algs$V1[i], mac$V1[i], mic$V1[i], time$V1[i], time$V2[i], nc$V1[i], j))
+            i <- i + 1
+            
+        }
+        j <- j+4
+    } 
+    
+    names <- c("Algorithm", "MacroF1", "MicroF1", "TrainTime", "TestTime", "NumNewClasses", "Perc")  
+    colnames(dataFrame) <- names
+    dataFrame
+}
+
+plotMacMic <- function(data, base, perc){
+    mat <- data.matrix(data[,2:3])
+    rownames(mat) <- data$Algorithm
+    mat <- t(mat)
+    title <- paste("Macro e Micro F1: Base de dados ", base, "com ", perc*10 ,"% de treino.") 
+    barplot(mat, beside=TRUE, col=c("steelblue4","tomato"), ylab = " % ", las=2, yaxp=c(0,100,10))
+    # setup for no margins on the legend
+    title(title, line = 3)
+    legend('topright', rownames(mat), fill =c("steelblue4","tomato"))
+    
+}
+
+
+plotTime <- function(data, base, perc){
+    mat <- data.matrix(data[,4:5])
+    rownames(mat) <- data$Algorithm
+    mat <- t(mat)
+    title <- paste("Tempo: Base de dados ", base, "com ", perc*10 ,"% de treino.") 
+    barplot(mat, col=c("steelblue4","tomato"), ylab = "Tempo (s)", las=2)
+    title(title, line = 3)
+    legend('top', rownames(mat), fill =c("steelblue4","tomato") )
+}
